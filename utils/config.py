4d9bf66ed052ee8eb8ad8ff488dde33afadcc020
gen-gen33 <matsug08@gmail.com>
2025-03-22 06:52:19 +0900

    fix

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,5 @@
 import multiprocessing
 
-NUM_WORKERS = min(8, multiprocessing.cpu_count() - 1)
+NUM_WORKERS = min(16, multiprocessing.cpu_count() - 1)
 print(f"cpu count: {multiprocessing.cpu_count()}")
 print(f"number of workers: {NUM_WORKERS}")
